170f0dd85db3ce1f008c4a2b69092985c1d23f91
teddiur <ryaqueiroz@gmail.com>
2020-12-30 22:37:50 -0300

    styles of search input

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,7 +1,18 @@
+import { useRef, useState } from 'react';
 import { MarvelLogo } from '../../assets/index';
 import * as S from './styledLayout';
 
 const Layout = ({ onClick, children }) => {
+  const useFocus = () => {
+    const htmlElRef = useRef(null);
+    const setFocus = () => {
+      htmlElRef.current && htmlElRef.current.focus();
+    };
+    return [htmlElRef, setFocus];
+  };
+  const [inputRef, setInputFocus] = useFocus();
+  const [query, setQuery] = useState('');
+  console.log(inputRef);
   return (
     <S.Wrapper>
       <S.Header>
@@ -10,6 +21,33 @@ const Layout = ({ onClick, children }) => {
           src={MarvelLogo}
           alt="Logo Marvel"
         />
+        <S.Searchbar>
+          <input
+            ref={inputRef}
+            onChange={(event) => {
+              setQuery(event.target.value);
+              console.log(inputRef);
+            }}
+          />
+          <svg
+            onClick={setInputFocus}
+            width="48"
+            height="47"
+            viewBox="0 0 48 47"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="17.5" cy="17.5" r="16" strokeWidth="3" />
+            <line
+              x1="29.0399"
+              y1="27.9186"
+              x2="46.7176"
+              y2="45.5963"
+              strokeWidth="3"
+            />
+          </svg>
+          <span>X</span>
+        </S.Searchbar>
       </S.Header>
       <S.Main>{children}</S.Main>
     </S.Wrapper>
--- src/components/Layout/styledLayout.js
+++ src/components/Layout/styledLayout.js
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 const Header = styled.header`
   height: clamp(40px, 8vh, 70px);
-  background-color: white;
   width: 100%;
   background: black;
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 `;
 
 const Logo = styled.img`
@@ -32,4 +32,40 @@ const Wrapper = styled.div`
   justify-content: flex-start;
   flex-wrap: nowrap;
 `;
-export { Header, Logo, Main, Wrapper };
+
+const Searchbar = styled.div`
+  max-width: 25%;
+  box-sizing: border-box;
+  position: absolute;
+  right: 2%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    position: absolute;
+    width: 20px;
+    stroke: #802733;
+    right: 0;
+    cursor: pointer;
+  }
+  input:focus + svg {
+    left: 0;
+  }
+
+  input {
+    background-color: transparent;
+    border: none;
+    height: 50%;
+    width: 100%;
+    padding: 0 25px;
+    color: #a8a8a8;
+    caret-color: #802733;
+
+    &:focus {
+      outline: none;
+    }
+  }
+`;
+export { Header, Logo, Main, Wrapper, Searchbar };
