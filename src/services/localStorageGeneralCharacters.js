e82a96cafd105cc155d5317a6db649d6ab606384
teddiur <ryaqueiroz@gmail.com>
2021-01-03 03:24:09 -0300

    test netlify

--- src/services/localStorageGeneralCharacters.js
+++ src/services/localStorageGeneralCharacters.js
@@ -14,6 +14,7 @@ const makeRequest = async (offset, cancel) => {
 };
 
 export default async function getData(offset, cancel, days = 1) {
+  console.log(myStorage);
   let localResults = myStorage.getItem('results')
     ? JSON.parse(myStorage.getItem('results'))
     : [];
